986345e02f6e9da9b676d619b0f947814d9a790f
dhyanpandya42 <126542701+dhyanpandya42@users.noreply.github.com>
2024-11-09 12:07:33 -0600

    requirments

---
+++ dhyan/Table1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+    department VARCHAR(100)
+);
--- dhyan/project/app.py
+++ dhyan/project/app.py
@@ -1,10 +1,19 @@
-from flask import  Flask,render_template
-
+from flask import  Flask,render_template,jsonify
+# categories=[
+#     {"1":"Food",
+#     "2":"Transportation",
+#     "3":"Entertainment"
+#     }
+# ]
 
 app=Flask(__name__)
 @app.route("/")
 def helloapp():
-    return render_template('home.html')
+    return render_template('home.html',expensecategory=categories)
+
+# @app.route("/expense")
+# def list_expense():
+#     return jsonify(categories)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",debug=True)
